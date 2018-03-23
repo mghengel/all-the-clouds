8d8f8c5bf8badbdaf6427b7b6debe0c5e6f884d9
Travis Nelson <travis.o.nelson@gmail.com>
2018-03-22 21:35:33 -0600

    added static file paths to server

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
