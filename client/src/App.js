fce88d2773adae6f7135951fa3c462fa4b55bba3
Mark <mhengel@i3logix.com>
2018-03-22 22:58:46 -0600

    Porting awesomness

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,9 @@
 import React, { Component } from 'react';
-
 import logo from './logo.svg';
-
 import './App.css';
+import Cloud from './cloud.js';
 
 class App extends Component {
-  state = {
-    test: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ test: res.test }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/aws-serverless-js');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
   render() {
     return (
       <div className="App">
@@ -31,7 +11,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.test}</p>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Cloud name="AWS" api='/api/aws-serverless-js' />
+        <Cloud name="Heroku" api='/test.json' />
+        <Cloud name="azure" api='/test.json' />
+        <Cloud name="Google Cloud" api='/test.json' />
       </div>
     );
   }
