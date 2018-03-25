cba03a21eea2d028866ff533c9b263b5ed1513cf
Travis Nelson <travis.o.nelson@gmail.com>
2018-03-24 21:22:20 -0600

    cleaned up microservices calls

--- server.js
+++ server.js
@@ -12,54 +12,34 @@ app.get('/api/hello', (req, res) => {
   res.send({ test: 'Hello From Express' });
 });
 
-app.get('/api/aws-serverless-js', (req, res) => {
-  request('https://fp55uw8uu8.execute-api.us-east-1.amazonaws.com/dev/', function (error, response, body) {
-  	if(error || response.statusCode !== 200) {
-  	  res.json({'color': 'yellow'});
-  	} else {
-  	  res.send(body);
-  	}
+callApi = (url, res) => {
+  return request(url, function (error, response, body) {
+    if(error || response.statusCode !== 200) {
+      res.json(error);
+    } else {
+      res.send(body);
+    }
   });
+};
+
+app.get('/api/aws-serverless-js', (req, res) => {
+  callApi('https://fp55uw8uu8.execute-api.us-east-1.amazonaws.com/dev', res);
 });
 
 app.get('/api/heroku-clojure', (req, res) => {
-  request('https://all-the-clouds-heroku-clojure.herokuapp.com', function (error, response, body) {
-  	if(error || response.statusCode !== 200) {
-  	  res.json({'color': 'yellow'});
-  	} else {
-  	  res.send(body);
-  	}
-  });
+  callApi('https://all-the-clouds-heroku-clojure.herokuapp.com', res);
 });
 
 app.get('/api/gcp-python', (req, res) => {
-  request('http://all-the-clouds-gcp-python.appspot.com', function (error, response, body) {
-  	if(error || response.statusCode !== 200) {
-  	  res.json({'color': 'yellow'});
-  	} else {
-  	  res.send(body);
-  	}
-  });
+  callApi('http://all-the-clouds-gcp-python.appspot.com', res);
 });
 
 app.get('/api/azure-dotnet', (req, res) => {
-  request('https://all-the-clouds-azure-dotnet.azurewebsites.net/api/values/1', function (error, response, body) {
-  	if(error || response.statusCode !== 200) {
-  	  res.json({'color': 'yellow'});
-  	} else {
-  	  res.send(body);
-  	}
-  });
+  callApi('https://all-the-clouds-azure-dotnet.azurewebsites.net/api/values/1', res);
 });
 
 app.get('/api/openshift-ruby', (req, res) => {
-  request('http://openshift-ruby-demo-all-the-clouds-openshift-ruby.7e14.starter-us-west-2.openshiftapps.com', function (error, response, body) {
-  	if(error || response.statusCode !== 200) {
-  	  res.json({'color': 'yellow'});
-  	} else {
-  	  res.send(body);
-  	}
-  });
+  callApi('http://openshift-ruby-demo-all-the-clouds-openshift-ruby.7e14.starter-us-west-2.openshiftapps.com', res);
 });
 
 
